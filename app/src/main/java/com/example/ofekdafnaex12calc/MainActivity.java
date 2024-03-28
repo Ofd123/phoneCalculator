51c4420fd0b49eb5d09a1a24c3032d1cb4fe76e4
Cyber_User <ad6354@bs.amalnet.k12.il>
2024-03-28 21:11:08 +0200

    changed a bit left to do: 1. oppose the calc option (devide by and then add num) 2. fix bugs(do smth but no input) 3. add credits

--- app/src/main/java/com/example/ofekdafnaex12calc/MainActivity.java
+++ app/src/main/java/com/example/ofekdafnaex12calc/MainActivity.java
@@ -10,8 +10,10 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     EditText edt;
     Button btn, btn1, btn2, btn3, btn4, btn5, btn6;
-    int number;
-
+    double inputNum = 0;
+    double number = 0;
+    String input;
+    boolean plus1 = false, minus1 = false, multiply1 = false,  devide1 = false;
     @Override
 
     protected void onCreate(Bundle savedInstanceState)
@@ -30,14 +32,84 @@ public class MainActivity extends AppCompatActivity {
 
     public void problem(View view)
     {
+        if (number == 0)
+        {
+            input = edt.getText().toString();
+        }
+        else
+        {
+            input = String.valueOf(number);
+        }
+        if (!(input.isEmpty()))
+        {
+            edt.setHint(input);
+            edt.setText("");
+            //------------------------'
+
+            inputNum = Double.parseDouble(input);
+            if (plus1)
+            {
+                number += inputNum;
+                plus1 = false;
+            }
+            if (minus1)
+            {
+                number -= inputNum;
+                minus1 = false;
+            }
+            if (multiply1)
+            {
+                number *= inputNum;
+                multiply1 = false;
+            }
+            if (devide1)
+            {
+                number /= inputNum;
+                devide1 = false;
+            }
+        }
     }
 
-    public void plus(View view) {
+
+    //----------------------------------------------------------------
+    public void plus(View view)
+    {
+        problem(view);
+        plus1 = true;
+    }
+    //----------------------------------------------------------------
+    public void minus(View view)
+    {
+        problem(view);
+        minus1 = true;
+    }
+    //----------------------------------------------------------------
+    public void devide(View view)
+    {
+        problem(view);
+        devide1 = true;
+    }
+    //----------------------------------------------------------------
+    public void multiply(View view)
+    {
+        problem(view);
+        multiply1 = true;
+    }
+    //----------------------------------------------------------------
+    public void answer(View view)
+    {
+        problem(view);
+    }
+    //----------------------------------------------------------------
+    public void reset(View view)
+    {
+        number = 0;
+        input = "0";
+        edt.setHint(input);
+    }
+    //----------------------------------------------------------------
+    public void credits(View view)
+    {
     }
 
-    public void minus(View view) {
-    }
-
-    public void device(View view) {
-    }
 }
